fb30c0e4d3b0fd3f98fabdf29a06469ff66f08ae
Marcus Weinberger <marcusjw.ftw@gmail.com>
2023-02-19 22:45:25 -0500

    lol so

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function getAccessToken(cb) {
+    // Replace with your own client ID and secret
+    const CLIENT_ID = '13bbc4d460b944fa83ed8b3ac5cdec76';
+    const CLIENT_SECRET = 'f843b85ebe464b5180801a89a1cfe403';
+    fetch('https://accounts.spotify.com/api/token', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+      },
+      body: 'grant_type=client_credentials',
+    })
+    .then(response => response.json())
+    .then(data => {
+      cb(data.access_token);
+    });
+  }
+
 // Initialize the Spotify Web API
 window.onSpotifyWebPlaybackSDKReady = () => {
     const player = new Spotify.Player({
@@ -23,7 +41,6 @@ window.onSpotifyWebPlaybackSDKReady = () => {
   
     // Connect to the Spotify Web Player
     player.connect();
-    globalThis.sp_player = player;
   };
   
   // Log in to Spotify
@@ -42,7 +59,7 @@ window.onSpotifyWebPlaybackSDKReady = () => {
   const extractButton = document.getElementById('extract-button');
   extractButton.addEventListener('click', () => {
     // Replace with your own access token
-    sp_player.getOAuthToken((accessToken => {
+    getAccessToken((accessToken => {
         const playlistId = document.getElementById('playlist-dropdown').value;
         fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
           headers: {
@@ -67,7 +84,7 @@ window.onSpotifyWebPlaybackSDKReady = () => {
   // Populate the playlist dropdown with the user's playlists
   function loadPlaylists() {
     // Replace with your own access token
-    sp_player.getOAuthToken((accessToken => {
+    getAccessToken((accessToken => {
         fetch('https://api.spotify.com/v1/me/playlists', {
             headers: {
               'Authorization': `Bearer ${accessToken}`,
